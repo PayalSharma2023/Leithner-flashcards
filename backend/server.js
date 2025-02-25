29179186fa4beed33d8c11a03977120972190666
Payal Sharma <payalsharmareal@gmail.com>
2025-02-25 14:26:40 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,29 @@ const cors = require("cors");
 const DbConnect = require("./config/db");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log("Origin:", origin); // Log incoming origin for debugging
+      const allowedOrigins = [
+        "http://localhost:5173","https://leithner-flashcards.vercel.app"
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization", "x-rtb-fingerprint-id"], // Allowing specific headers if necessary
+    credentials: true, // Enable sending cookies, if necessary
+  })
+);
+const corsOptions = {
+  origin: ["http://localhost:5173","https://leithner-flashcards.vercel.app"], // Your Vercel frontend domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
 app.use("/flashcards", require("./routes/flashcards"));
 app.use('/auth', require('./routes/auth'));
