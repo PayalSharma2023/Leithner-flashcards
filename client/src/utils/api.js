a48566be64473780619c9f3c7ed66f98b48f3dc9
Payal Sharma <payalsharmareal@gmail.com>
2025-02-25 13:32:23 +0530

    updates

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/flashcards";
+const API_URL = import.meta.env.VITE_BACKEND_URI;
 
 export const getFlashcards = async () => {
   try {
